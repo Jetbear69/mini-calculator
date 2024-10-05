1a58ab88ad011ac8124803d0af850002872d7687
JetBear69 <ohuamanm@gmail.com>
2024-10-05 18:04:41 -0500

    mini calculator project

---
+++ script.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', function (){
+
+    let historyDiv = document.querySelector('.history');
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let history = '';
+
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            handleButtonClick(button.innerText);
+        });
+    });
+    
+    function handleButtonClick(value) {
+        if(value === 'AC'){
+            clearAll();
+        }
+        else if(value === 'DEL') {
+            deleteLastChar();
+        }
+        else if(value === '=') {
+            evaluateExpression();
+        }
+        else if (value === '%') {
+            applyPercentage();
+        }
+        else {
+            appendToScreen(value);
+        }
+    }
+
+    function clearAll() {
+        screen.textContent = '';
+        history = '';
+        updateHistory();
+    }
+
+    function deleteLastChar() {
+        let currentText= screen.textContent;
+        screen.textContent = currentText.slice (0, -1);
+    }
+
+    function appendToScreen(value) {
+        screen.textContent += value;
+    }
+
+    function evaluateExpression() {
+        try{
+            let expression = screen.textContent;
+            let result = eval(expression);
+
+            result = parseFloat(result.toFixed(5));
+
+            history = expression + ' = ' + result;
+
+            screen.textContent = result;
+            updateHistory();
+        }   catch(error){
+                screen.textContent = 'Error';
+            }
+    }
+
+    // Function ApplyPercentage Define Below this line
+
+    function applyPercentage(){
+        try{
+            let expression = screen.textContent;
+            let result = eval(expression + '*100');
+
+            result = parseFloat(result.toFixed(5));
+            history = expression + '% = ' + result;
+
+            screen.textContent = result;
+            updateHistory();
+        }   catch (error){
+                screen.textContent = 'Error';
+            }
+        //catch is used to catch Error
+    }
+
+    function updateHistory(){
+        historyDiv.textContent = history;
+    }
+})
